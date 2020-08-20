b31e0ed07fb2ff018f3835a5c37ba0b4d97414e1
Fabian Ramirez <fabian.ramirez@thet.com.ar>
2020-08-20 20:33:00 -0300

    adding update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,14 @@
 const numbers = [1, 2, 3, 4];
-const numbersDouble = numbers.map(number => number * 2);
+const numbersDouble = numbers.map(number => number * 2)
+
+class Persona {
+    constructor (nombre) {
+        this.nombre = nombre;
+    }
+};
 
 console.log(numbers);
+
+console.log('linea de prueba');
+
+console.log('cuarta linea de prueba');
